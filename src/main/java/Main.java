ae1c842bc1ea370e9d1cb58979348b3bc0fe6aa7
Siddhi Soman <ssoman@slack-corp.com>
2016-12-09 11:43:54 -0800

    Fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -66,7 +66,7 @@ public class Main extends AbstractHandler {
 		tokens = text.split(" ");
 		op = tokens[1];
 		}
-		if(op.equals("search") || op == null || op.isEmpty()){
+		if(op == null || op.equals("search") ||  op.isEmpty()){
 		FBGraph fbGraph = new FBGraph("access_token=EAAZAirncyLTwBAHUW6tGXxJ3YhlhkzmnExHp7irya5kw8Fu7ZBJzumiXoq0aZCa7UAk5GDziwJbZCGGnvkAVx6hZAlQFkSKqALZAbqd1KujNKtXUgWlZAfK9ZCUmTcGxWnpdpv6R1zytzcF97rugFONX84tsDS5EqkIHgvdLi58aVmjKxPGqrAww&expires_in=6429");
 		String graph = fbGraph.getFBGraph();
 		Map<String, String> fbProfileData = fbGraph.getGraphData(graph);
